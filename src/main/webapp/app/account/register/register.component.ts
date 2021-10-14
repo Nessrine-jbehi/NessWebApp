ec78e0e9e404d49a90d42d2e66986a433056fba9
nessrine-jbehi <aziza@hosinox.com>
2021-10-15 01:41:17 +0200

    cleaning

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -1,8 +1,5 @@
-import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-
-import { EMAIL_ALREADY_USED_TYPE, LOGIN_ALREADY_USED_TYPE } from 'app/config/error.constants';
 import { RegisterService } from './register.service';
 
 @Component({
@@ -10,15 +7,8 @@ import { RegisterService } from './register.service';
   templateUrl: './register.component.html',
   styleUrls: ['./style.scss'],
 })
-export class RegisterComponent implements AfterViewInit {
-  @ViewChild('login', { static: false })
-  login?: ElementRef;
-
-  doNotMatch = false;
+export class RegisterComponent {
   error = false;
-  errorEmailExists = false;
-  errorUserExists = false;
-  success = false;
 
   registerForm = this.fb.group({
     email: ['', [Validators.minLength(5), Validators.maxLength(254), Validators.email]],
@@ -27,38 +17,7 @@ export class RegisterComponent implements AfterViewInit {
 
   constructor(private registerService: RegisterService, private fb: FormBuilder) {}
 
-  ngAfterViewInit(): void {
-    if (this.login) {
-      this.login.nativeElement.focus();
-    }
-  }
-
   register(): void {
-    this.doNotMatch = false;
     this.error = false;
-    this.errorEmailExists = false;
-    this.errorUserExists = false;
-
-    const password = this.registerForm.get(['password'])!.value;
-    if (password !== this.registerForm.get(['confirmPassword'])!.value) {
-      this.doNotMatch = true;
-    } else {
-      const login = this.registerForm.get(['login'])!.value;
-      const email = this.registerForm.get(['email'])!.value;
-      this.registerService.save({ login, email, password, langKey: 'en' }).subscribe(
-        () => (this.success = true),
-        response => this.processError(response)
-      );
-    }
-  }
-
-  private processError(response: HttpErrorResponse): void {
-    if (response.status === 400 && response.error.type === LOGIN_ALREADY_USED_TYPE) {
-      this.errorUserExists = true;
-    } else if (response.status === 400 && response.error.type === EMAIL_ALREADY_USED_TYPE) {
-      this.errorEmailExists = true;
-    } else {
-      this.error = true;
-    }
   }
 }
